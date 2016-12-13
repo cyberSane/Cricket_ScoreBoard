3682bd5f840cf91e18ce47b7d348d8f299b35ae8
Akshay <kumarsharmaa69@gmail.com>
2016-12-13 18:22:27 +0530

    Written test of runs method of Ball class

--- src/test/scala/ball/BallTest.scala
+++ src/test/scala/ball/BallTest.scala
@@ -3,21 +3,28 @@ package ball
 import org.scalatest.FunSuite
 
 class BallTest extends FunSuite {
-  test("ball should tell if it is a odd run ball") {
+  test("should tell if it is a odd run ball") {
     val ballWith1Run = new Ball("1")
-    val ballWith2Run = new Ball("2")
+    val ballWith2Runs = new Ball("2")
 
     assertResult(true)(ballWith1Run.isOddRun)
-    assertResult(false)(ballWith2Run.isOddRun)
+    assertResult(false)(ballWith2Runs.isOddRun)
   }
 
-  test("ball should tell if it is a wicket ball") {
+  test("should tell if it is a wicket ball") {
     val ballWith1Run = new Ball("1")
-    val ballWith2Run = new Ball("W")
+    val ballWith2Runs = new Ball("W")
 
     assertResult(false)(ballWith1Run.isWicket)
-    assertResult(true)(ballWith2Run.isWicket)
+    assertResult(true)(ballWith2Runs.isWicket)
   }
 
+  test("should return runs scored on that ball"){
+    val ballWith1Run = new Ball("1")
+    val ballWith6Runs = new Ball("6")
+
+    assertResult(1)(ballWith1Run.runs)
+    assertResult(6)(ballWith6Runs.runs)
+  }
 
 }

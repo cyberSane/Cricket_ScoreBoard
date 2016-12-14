a25045ac63948a95c72137df206b0678b4103fae
Akshay <kumarsharmaa69@gmail.com>
2016-12-14 08:39:28 +0530

    created Balls collection class and added get method in it to get a specific ball

---
+++ src/main/scala/ball/Balls.scala
@@ -0,0 +1,21 @@
+package ball
+
+import java.util.Observable
+
+class Balls(balls: Array[Ball]) extends Observable {
+
+  def isLastBall(ball: Ball): Boolean = {
+    (balls.indexOf(ball) + 1) % 6 == 0
+  }
+
+  def get(ballNumber: Int): Ball = {
+    val ball: Ball = balls(ballNumber)
+
+    if (isLastBall(ball)) {
+      setChanged()
+      notifyObservers(true)
+    }
+    ball
+  }
+
+}
---
+++ src/test/scala/ball/BallsTest.scala
@@ -0,0 +1,33 @@
+package ball
+
+import java.util.Observable
+
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+class BallsTest extends FunSuite with BeforeAndAfter with MockitoSugar {
+  var balls: Balls = _
+  val firstBall: Ball = new Ball("1")
+  val secondBall: Ball = new Ball("2")
+  val thirdBall: Ball = new Ball("W")
+  val fourthBall: Ball = new Ball("1")
+  val fifthBall: Ball = new Ball("4")
+  val sixthBall: Ball = new Ball("3")
+
+  before {
+    val ballsArray = Array(firstBall,
+      secondBall,
+      thirdBall,
+      fourthBall,
+      firstBall,
+      sixthBall
+    )
+    balls = new Balls(ballsArray)
+  }
+  test("get should give the ball and notify nothing when ball is not the last ball of an over") {
+    val expectedBall: Ball = thirdBall
+    
+    assertResult(expectedBall)(balls.get(2))
+  }
+}
